469116ab21f76338d262da6a921e0008e88f8389
Matt Jenkins <matt@majenko.co.uk>
2015-02-23 14:12:16 +0000

    Added ArialBold font

---
+++ Fonts/ArialBold/ArialBold.h
@@ -0,0 +1,26 @@
+#ifndef _ARIALBOLD_H
+#define _ARIALBOLD_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+        extern const uint8_t ArialBold10[];
+        extern const uint8_t ArialBold12[];
+        extern const uint8_t ArialBold14[];
+        extern const uint8_t ArialBold16[];
+        extern const uint8_t ArialBold18[];
+        extern const uint8_t ArialBold20[];
+        extern const uint8_t ArialBold22[];
+        extern const uint8_t ArialBold24[];
+        extern const uint8_t ArialBold26[];
+        extern const uint8_t ArialBold28[];
+        extern const uint8_t ArialBold30[];
+        extern const uint8_t ArialBold32[];
+        extern const uint8_t ArialBold34[];
+        extern const uint8_t ArialBold36[];
+        extern const uint8_t ArialBold38[];
+        extern const uint8_t ArialBold40[];
+        extern const uint8_t ArialBold8[];
+};
+
+#endif
