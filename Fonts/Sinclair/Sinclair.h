811e8c328772dc70007b87b3bd2abf2b5a715900
Matt Jenkins <matt@majenko.co.uk>
2015-02-23 12:13:18 +0000

    Added Sinclair and Ubuntu fonts

---
+++ Fonts/Sinclair/Sinclair.h
@@ -0,0 +1,10 @@
+#ifndef _SINCLAIR_H
+#define _SINCLAIR_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+        extern const uint8_t Sinclair[];
+};
+
+#endif
---
+++ Fonts/Ubuntu/Ubuntu.h
@@ -0,0 +1,28 @@
+#ifndef _UBUNTU_H
+#define _UBUNTU_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+        extern const uint8_t Ubuntu10[];
+        extern const uint8_t Ubuntu12[];
+        extern const uint8_t Ubuntu14[];
+        extern const uint8_t Ubuntu16[];
+        extern const uint8_t Ubuntu18[];
+        extern const uint8_t Ubuntu20[];
+        extern const uint8_t Ubuntu22[];
+        extern const uint8_t Ubuntu24[];
+        extern const uint8_t Ubuntu26[];
+        extern const uint8_t Ubuntu28[];
+        extern const uint8_t Ubuntu30[];
+        extern const uint8_t Ubuntu32[];
+        extern const uint8_t Ubuntu34[];
+        extern const uint8_t Ubuntu36[];
+        extern const uint8_t Ubuntu38[];
+        extern const uint8_t Ubuntu40[];
+        extern const uint8_t Ubuntu42[];
+        extern const uint8_t Ubuntu44[];
+        extern const uint8_t Ubuntu8[];
+};
+
+#endif
