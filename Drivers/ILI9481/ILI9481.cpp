5e2799de03bbf31630e394f3868b0d29b9efbf5c
Matt Jenkins <matt@majenko.co.uk>
2015-03-17 17:54:35 +0000

    Fixed main versino of ILI9841

--- Drivers/ILI9481/ILI9481.cpp
+++ Drivers/ILI9481/ILI9481.cpp
@@ -1,47 +1,135 @@
 #include <ILI9481.h>
 
-void ILI9481::command(uint16_t cmd) {
-    _dcp->lat.clr = _dcb;
-    _csp->lat.clr = _csb;
-    _spi->transfer(cmd);
-    _csp->lat.set = _csb;
+ILI9481::ILI9481(
+    uint8_t rs, uint8_t wr, uint8_t rd, uint8_t cs, uint8_t reset,
+    uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+    uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
+    uint8_t d8, uint8_t d9, uint8_t d10, uint8_t d11,
+    uint8_t d12, uint8_t d13, uint8_t d14, uint8_t d15
+) {
+    pin_rs = rs;
+    pin_wr = wr;
+    pin_rd = rd;
+    pin_cs = cs;
+    pin_reset = reset;
+    pin_d0 = d0;
+    pin_d1 = d1;
+    pin_d2 = d2;
+    pin_d3 = d3;
+    pin_d4 = d4;
+    pin_d5 = d5;
+    pin_d6 = d6;
+    pin_d7 = d7;
+    pin_d8 = d8;
+    pin_d9 = d9;
+    pin_d10 = d10;
+    pin_d11 = d11;
+    pin_d12 = d12;
+    pin_d13 = d13;
+    pin_d14 = d14;
+    pin_d15 = d15;
 }
 
-void ILI9481::data(uint16_t dat) {
-    _dcp->lat.set = _dcb;
-    _csp->lat.clr = _csb;
-    _spi->transfer(dat);
-    _csp->lat.set = _csb;
+void ILI9481::command(uint16_t com) {
+    port_rs->lat.clr = mask_rs;
+    com & 0x0001 ? port_d0->lat.set = mask_d0 : port_d0->lat.clr = mask_d0;
+    com & 0x0002 ? port_d1->lat.set = mask_d1 : port_d1->lat.clr = mask_d1;
+    com & 0x0004 ? port_d2->lat.set = mask_d2 : port_d2->lat.clr = mask_d2;
+    com & 0x0008 ? port_d3->lat.set = mask_d3 : port_d3->lat.clr = mask_d3;
+    com & 0x0010 ? port_d4->lat.set = mask_d4 : port_d4->lat.clr = mask_d4;
+    com & 0x0020 ? port_d5->lat.set = mask_d5 : port_d5->lat.clr = mask_d5;
+    com & 0x0040 ? port_d6->lat.set = mask_d6 : port_d6->lat.clr = mask_d6;
+    com & 0x0080 ? port_d7->lat.set = mask_d7 : port_d7->lat.clr = mask_d7;
+    com & 0x0100 ? port_d8->lat.set = mask_d8 : port_d8->lat.clr = mask_d8;
+    com & 0x0200 ? port_d9->lat.set = mask_d9 : port_d9->lat.clr = mask_d9;
+    com & 0x0400 ? port_d10->lat.set = mask_d10 : port_d10->lat.clr = mask_d10;
+    com & 0x0800 ? port_d11->lat.set = mask_d11 : port_d11->lat.clr = mask_d11;
+    com & 0x1000 ? port_d12->lat.set = mask_d12 : port_d12->lat.clr = mask_d12;
+    com & 0x2000 ? port_d13->lat.set = mask_d13 : port_d13->lat.clr = mask_d13;
+    com & 0x4000 ? port_d14->lat.set = mask_d14 : port_d14->lat.clr = mask_d14;
+    com & 0x8000 ? port_d15->lat.set = mask_d15 : port_d15->lat.clr = mask_d15;
+    port_wr->lat.clr = mask_wr;
+    port_wr->lat.set = mask_wr;
 }
+
+void ILI9481::data(uint16_t com) {
+    port_rs->lat.set = mask_rs;
+    com & 0x0001 ? port_d0->lat.set = mask_d0 : port_d0->lat.clr = mask_d0;
+    com & 0x0002 ? port_d1->lat.set = mask_d1 : port_d1->lat.clr = mask_d1;
+    com & 0x0004 ? port_d2->lat.set = mask_d2 : port_d2->lat.clr = mask_d2;
+    com & 0x0008 ? port_d3->lat.set = mask_d3 : port_d3->lat.clr = mask_d3;
+    com & 0x0010 ? port_d4->lat.set = mask_d4 : port_d4->lat.clr = mask_d4;
+    com & 0x0020 ? port_d5->lat.set = mask_d5 : port_d5->lat.clr = mask_d5;
+    com & 0x0040 ? port_d6->lat.set = mask_d6 : port_d6->lat.clr = mask_d6;
+    com & 0x0080 ? port_d7->lat.set = mask_d7 : port_d7->lat.clr = mask_d7;
+    com & 0x0100 ? port_d8->lat.set = mask_d8 : port_d8->lat.clr = mask_d8;
+    com & 0x0200 ? port_d9->lat.set = mask_d9 : port_d9->lat.clr = mask_d9;
+    com & 0x0400 ? port_d10->lat.set = mask_d10 : port_d10->lat.clr = mask_d10;
+    com & 0x0800 ? port_d11->lat.set = mask_d11 : port_d11->lat.clr = mask_d11;
+    com & 0x1000 ? port_d12->lat.set = mask_d12 : port_d12->lat.clr = mask_d12;
+    com & 0x2000 ? port_d13->lat.set = mask_d13 : port_d13->lat.clr = mask_d13;
+    com & 0x4000 ? port_d14->lat.set = mask_d14 : port_d14->lat.clr = mask_d14;
+    com & 0x8000 ? port_d15->lat.set = mask_d15 : port_d15->lat.clr = mask_d15;
+    port_wr->lat.clr = mask_wr;
+    port_wr->lat.set = mask_wr;
+}
+
 
 void ILI9481::initializeDevice() {
-    if (_cs >= NUM_DIGITAL_PINS_EXTENDED) {
-        return;
-    }
-    if (_dc >= NUM_DIGITAL_PINS_EXTENDED) {
-        return;
-    }
-    _spi->begin();
-    _spi->setSpeed(20000000UL);
+    pinMode(pin_rs, OUTPUT);
+    pinMode(pin_cs, OUTPUT);
+    pinMode(pin_rd, OUTPUT);
+    pinMode(pin_wr, OUTPUT);
+    pinMode(pin_reset, OUTPUT);
+    pinMode(pin_d0, OUTPUT);
+    pinMode(pin_d1, OUTPUT);
+    pinMode(pin_d2, OUTPUT);
+    pinMode(pin_d3, OUTPUT);
+    pinMode(pin_d4, OUTPUT);
+    pinMode(pin_d5, OUTPUT);
+    pinMode(pin_d6, OUTPUT);
+    pinMode(pin_d7, OUTPUT);
+    pinMode(pin_d8, OUTPUT);
+    pinMode(pin_d9, OUTPUT);
+    pinMode(pin_d10, OUTPUT);
+    pinMode(pin_d11, OUTPUT);
+    pinMode(pin_d12, OUTPUT);
+    pinMode(pin_d13, OUTPUT);
+    pinMode(pin_d14, OUTPUT);
+    pinMode(pin_d15, OUTPUT);
 
-    pinMode(_cs, OUTPUT);
-    pinMode(_dc, OUTPUT);
+    port_rs = getPortInformation(pin_rs, &mask_rs);
+    port_rd = getPortInformation(pin_rd, &mask_rd);
+    port_wr = getPortInformation(pin_wr, &mask_wr);
+    port_d0 = getPortInformation(pin_d0, &mask_d0);
+    port_d1 = getPortInformation(pin_d1, &mask_d1);
+    port_d2 = getPortInformation(pin_d2, &mask_d2);
+    port_d3 = getPortInformation(pin_d3, &mask_d3);
+    port_d4 = getPortInformation(pin_d4, &mask_d4);
+    port_d5 = getPortInformation(pin_d5, &mask_d5);
+    port_d6 = getPortInformation(pin_d6, &mask_d6);
+    port_d7 = getPortInformation(pin_d7, &mask_d7);
+    port_d8 = getPortInformation(pin_d8, &mask_d8);
+    port_d9 = getPortInformation(pin_d9, &mask_d9);
+    port_d10 = getPortInformation(pin_d10, &mask_d10);
+    port_d11 = getPortInformation(pin_d11, &mask_d11);
+    port_d12 = getPortInformation(pin_d12, &mask_d12);
+    port_d13 = getPortInformation(pin_d13, &mask_d13);
+    port_d14 = getPortInformation(pin_d14, &mask_d14);
+    port_d15 = getPortInformation(pin_d15, &mask_d15);
 
-    uint32_t port = digitalPinToPort(_cs);
-    if (port == NOT_A_PIN) {
-        return;
-    }
-    _csp = (p32_ioport *)portRegisters(port);
-    _csb = digitalPinToBitMask(_cs);
+    digitalWrite(pin_rs, HIGH);
+    digitalWrite(pin_cs, HIGH);
+    digitalWrite(pin_rd, HIGH);
+    digitalWrite(pin_wr, HIGH);
+    digitalWrite(pin_reset, HIGH);
+    delay(100);
+    digitalWrite(pin_reset, LOW);
+    delay(100);
+    digitalWrite(pin_reset, HIGH);
+    delay(100);
+    digitalWrite(pin_cs, LOW);
 
-    port = digitalPinToPort(_dc);
-    if (port == NOT_A_PIN) {
-        return;
-    }
-    _dcp = (p32_ioport *)portRegisters(port);
-    _dcb = digitalPinToBitMask(_dc);
-
-    _csp->lat.set = _csb;
     startDisplay();
 }
 
--- Drivers/ILI9481/ILI9481.h
+++ Drivers/ILI9481/ILI9481.h
@@ -2,25 +2,84 @@
 #define _ILI9481_H
 
 #include <DisplayCore.h>
-#include <DSPI.h>
 
 class ILI9481 : public DisplayCore {
     private:
-        DSPI *_spi;
-        uint8_t _cs;
-        uint8_t _dc;
-        p32_ioport *_csp;
-        p32_ioport *_dcp;
-        uint32_t _csb;
-        uint32_t _dcb;
+        uint8_t pin_rs;
+        uint8_t pin_rd;
+        uint8_t pin_wr;
+        uint8_t pin_cs;
+        uint8_t pin_reset;
+        uint8_t pin_d0;
+        uint8_t pin_d1;
+        uint8_t pin_d2;
+        uint8_t pin_d3;
+        uint8_t pin_d4;
+        uint8_t pin_d5;
+        uint8_t pin_d6;
+        uint8_t pin_d7;
+        uint8_t pin_d8;
+        uint8_t pin_d9;
+        uint8_t pin_d10;
+        uint8_t pin_d11;
+        uint8_t pin_d12;
+        uint8_t pin_d13;
+        uint8_t pin_d14;
+        uint8_t pin_d15;
 
-	public:
+        p32_ioport *port_rs;
+        p32_ioport *port_rd;
+        p32_ioport *port_wr;
+        p32_ioport *port_d0;
+        p32_ioport *port_d1;
+        p32_ioport *port_d2;
+        p32_ioport *port_d3;
+        p32_ioport *port_d4;
+        p32_ioport *port_d5;
+        p32_ioport *port_d6;
+        p32_ioport *port_d7;
+        p32_ioport *port_d8;
+        p32_ioport *port_d9;
+        p32_ioport *port_d10;
+        p32_ioport *port_d11;
+        p32_ioport *port_d12;
+        p32_ioport *port_d13;
+        p32_ioport *port_d14;
+        p32_ioport *port_d15;
+
+        uint32_t mask_rs;
+        uint32_t mask_rd;
+        uint32_t mask_wr;
+        uint32_t mask_d0;
+        uint32_t mask_d1;
+        uint32_t mask_d2;
+        uint32_t mask_d3;
+        uint32_t mask_d4;
+        uint32_t mask_d5;
+        uint32_t mask_d6;
+        uint32_t mask_d7;
+        uint32_t mask_d8;
+        uint32_t mask_d9;
+        uint32_t mask_d10;
+        uint32_t mask_d11;
+        uint32_t mask_d12;
+        uint32_t mask_d13;
+        uint32_t mask_d14;
+        uint32_t mask_d15;
+
+    public:
         static const uint16_t Width      = 320;
         static const uint16_t Height     = 480;
 
+        ILI9481(
+            uint8_t rs, uint8_t wr, uint8_t rd, uint8_t cs, uint8_t reset,
+            uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3,
+            uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7,
+            uint8_t d8, uint8_t d9, uint8_t d10, uint8_t d11,
+            uint8_t d12, uint8_t d13, uint8_t d14, uint8_t d15
+        );
+
         ILI9481() {}
-		ILI9481(DSPI *spi, uint8_t cs, uint8_t dc) : _spi(spi), _cs(cs), _dc(dc) {}
-		ILI9481(DSPI &spi, uint8_t cs, uint8_t dc) : _spi(&spi), _cs(cs), _dc(dc) {}
 
 		void setAddrWindow(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1);
         void fillScreen(uint16_t color);
