ed317370fa9462bd94f01985e0809f16b705e887
Matt Jenkins <matt@majenko.co.uk>
2015-03-17 18:58:20 +0000

    Added Aquarius and Arari fonts

---
+++ Fonts/Aquarius/Aquarius.h
@@ -0,0 +1,10 @@
+#ifndef _AQUARIUS_H
+#define _AQUARIUS_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Aquarius[];
+};
+
+#endif
---
+++ Fonts/Atari/Atari.h
@@ -0,0 +1,10 @@
+#ifndef _ATARI_H
+#define _ATARI_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Atari[];
+};
+
+#endif
