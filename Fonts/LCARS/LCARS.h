babec64a67a4fc6c0bd1085623531ac803c5c969
Matt Jenkins <matt@majenko.co.uk>
2015-03-28 16:34:34 +0000

    Added build files for new fonts

---
+++ Fonts/LCARS/LCARS.h
@@ -0,0 +1,25 @@
+#ifndef _LCARS_H
+#define _LCARS_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t LCARS8[];
+    extern const uint8_t LCARS10[];
+    extern const uint8_t LCARS12[];
+    extern const uint8_t LCARS14[];
+    extern const uint8_t LCARS16[];
+    extern const uint8_t LCARS18[];
+    extern const uint8_t LCARS20[];
+    extern const uint8_t LCARS22[];
+    extern const uint8_t LCARS24[];
+    extern const uint8_t LCARS26[];
+    extern const uint8_t LCARS28[];
+    extern const uint8_t LCARS30[];
+    extern const uint8_t LCARS32[];
+    extern const uint8_t LCARS34[];
+    extern const uint8_t LCARS36[];
+    extern const uint8_t LCARS38[];
+    extern const uint8_t LCARS40[];
+};
+#endif
