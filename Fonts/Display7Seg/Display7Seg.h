6406bb06eb1dd06e1075ddd882e02c69078fa748
Matt Jenkins <matt@majenko.co.uk>
2016-02-22 18:38:16 +0000

    Added Display7Seg font

---
+++ Fonts/Display7Seg/Display7Seg.h
@@ -0,0 +1,10 @@
+#ifndef _DISPLAY7SEG_H
+#define _DISPLAY7SEG_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Display7Seg32[];
+    extern const uint8_t Display7Seg48[];
+};
+#endif
