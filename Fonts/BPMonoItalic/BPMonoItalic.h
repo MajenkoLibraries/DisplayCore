9b857888f937908122465fc9c2a5d172538a7fc9
Matt Jenkins <matt@majenko.co.uk>
2016-04-15 01:10:58 +0100

    Added BPMonoItalic font

---
+++ Fonts/BPMonoItalic/BPMonoItalic.h
@@ -0,0 +1,20 @@
+#ifndef _BPMONOITALIC_H
+#define _BPMONOITALIC_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t BPMonoItalic20[];
+    extern const uint8_t BPMonoItalic22[];
+    extern const uint8_t BPMonoItalic24[];
+    extern const uint8_t BPMonoItalic26[];
+    extern const uint8_t BPMonoItalic28[];
+    extern const uint8_t BPMonoItalic30[];
+    extern const uint8_t BPMonoItalic32[];
+    extern const uint8_t BPMonoItalic34[];
+    extern const uint8_t BPMonoItalic36[];
+    extern const uint8_t BPMonoItalic38[];
+    extern const uint8_t BPMonoItalic40[];
+    extern const uint8_t BPMonoItalic18[];
+};
+#endif
