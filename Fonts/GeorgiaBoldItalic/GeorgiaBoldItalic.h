15e92e3241e164c2efdc3662b5d5f6538a0de228
Matt Jenkins <matt@majenko.co.uk>
2016-05-30 22:22:15 +0100

    Added GeorgiaBoldItalic

---
+++ Fonts/GeorgiaBoldItalic/GeorgiaBoldItalic.h
@@ -0,0 +1,37 @@
+#ifndef _GEORGIABOLDITALIC_H
+#define _GEORGIABOLDITALIC_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t GeorgiaBoldItalic8[];
+    extern const uint8_t GeorgiaBoldItalic10[];
+    extern const uint8_t GeorgiaBoldItalic12[];
+    extern const uint8_t GeorgiaBoldItalic14[];
+    extern const uint8_t GeorgiaBoldItalic16[];
+    extern const uint8_t GeorgiaBoldItalic18[];
+    extern const uint8_t GeorgiaBoldItalic20[];
+    extern const uint8_t GeorgiaBoldItalic22[];
+    extern const uint8_t GeorgiaBoldItalic24[];
+    extern const uint8_t GeorgiaBoldItalic26[];
+    extern const uint8_t GeorgiaBoldItalic28[];
+    extern const uint8_t GeorgiaBoldItalic30[];
+    extern const uint8_t GeorgiaBoldItalic32[];
+    extern const uint8_t GeorgiaBoldItalic34[];
+    extern const uint8_t GeorgiaBoldItalic36[];
+    extern const uint8_t GeorgiaBoldItalic38[];
+    extern const uint8_t GeorgiaBoldItalic40[];
+    extern const uint8_t GeorgiaBoldItalic42[];
+    extern const uint8_t GeorgiaBoldItalic44[];
+    extern const uint8_t GeorgiaBoldItalic46[];
+    extern const uint8_t GeorgiaBoldItalic48[];
+    extern const uint8_t GeorgiaBoldItalic50[];
+    extern const uint8_t GeorgiaBoldItalic52[];
+    extern const uint8_t GeorgiaBoldItalic54[];
+    extern const uint8_t GeorgiaBoldItalic56[];
+    extern const uint8_t GeorgiaBoldItalic58[];
+    extern const uint8_t GeorgiaBoldItalic60[];
+    extern const uint8_t GeorgiaBoldItalic62[];
+    extern const uint8_t GeorgiaBoldItalic64[];
+};
+#endif
