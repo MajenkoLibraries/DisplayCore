cf43df609e46b4bcb6a5ef00084bac6b2fde42b5
Matt Jenkins <matt@majenko.co.uk>
2017-02-15 16:17:14 +0000

    Re-rendered EngliBesh at 2bpp

--- Fonts/EngliBesh/EngliBesh.h
+++ Fonts/EngliBesh/EngliBesh.h
@@ -1,42 +1,41 @@
-#ifndef _FONTS_ENGLIBESH_H
-#define _FONTS_ENGLIBESH_H
+#ifndef _ENGLIBESH_H
+#define _ENGLIBESH_H
 
 #include <DisplayCore.h>
 
 namespace Fonts {
-        extern const uint8_t EngliBesh8[];
-        extern const uint8_t EngliBesh9[];
-        extern const uint8_t EngliBesh10[];
-        extern const uint8_t EngliBesh11[];
-        extern const uint8_t EngliBesh12[];
-        extern const uint8_t EngliBesh13[];
-        extern const uint8_t EngliBesh14[];
-        extern const uint8_t EngliBesh15[];
-        extern const uint8_t EngliBesh16[];
-        extern const uint8_t EngliBesh17[];
-        extern const uint8_t EngliBesh18[];
-        extern const uint8_t EngliBesh19[];
-        extern const uint8_t EngliBesh20[];
-        extern const uint8_t EngliBesh21[];
-        extern const uint8_t EngliBesh22[];
-        extern const uint8_t EngliBesh23[];
-        extern const uint8_t EngliBesh24[];
-        extern const uint8_t EngliBesh25[];
-        extern const uint8_t EngliBesh26[];
-        extern const uint8_t EngliBesh27[];
-        extern const uint8_t EngliBesh28[];
-        extern const uint8_t EngliBesh29[];
-        extern const uint8_t EngliBesh30[];
-        extern const uint8_t EngliBesh31[];
-        extern const uint8_t EngliBesh32[];
-        extern const uint8_t EngliBesh33[];
-        extern const uint8_t EngliBesh34[];
-        extern const uint8_t EngliBesh35[];
-        extern const uint8_t EngliBesh36[];
-        extern const uint8_t EngliBesh37[];
-        extern const uint8_t EngliBesh38[];
-        extern const uint8_t EngliBesh39[];
-        extern const uint8_t EngliBesh40[];
+    extern const uint8_t EngliBesh18[];
+    extern const uint8_t EngliBesh16[];
+    extern const uint8_t EngliBesh14[];
+    extern const uint8_t EngliBesh8[];
+    extern const uint8_t EngliBesh9[];
+    extern const uint8_t EngliBesh10[];
+    extern const uint8_t EngliBesh11[];
+    extern const uint8_t EngliBesh12[];
+    extern const uint8_t EngliBesh13[];
+    extern const uint8_t EngliBesh15[];
+    extern const uint8_t EngliBesh17[];
+    extern const uint8_t EngliBesh19[];
+    extern const uint8_t EngliBesh20[];
+    extern const uint8_t EngliBesh21[];
+    extern const uint8_t EngliBesh22[];
+    extern const uint8_t EngliBesh23[];
+    extern const uint8_t EngliBesh24[];
+    extern const uint8_t EngliBesh25[];
+    extern const uint8_t EngliBesh26[];
+    extern const uint8_t EngliBesh27[];
+    extern const uint8_t EngliBesh28[];
+    extern const uint8_t EngliBesh29[];
+    extern const uint8_t EngliBesh30[];
+    extern const uint8_t EngliBesh31[];
+    extern const uint8_t EngliBesh32[];
+    extern const uint8_t EngliBesh33[];
+    extern const uint8_t EngliBesh34[];
+    extern const uint8_t EngliBesh35[];
+    extern const uint8_t EngliBesh36[];
+    extern const uint8_t EngliBesh37[];
+    extern const uint8_t EngliBesh38[];
+    extern const uint8_t EngliBesh39[];
+    extern const uint8_t EngliBesh40[];
 };
-
 #endif
